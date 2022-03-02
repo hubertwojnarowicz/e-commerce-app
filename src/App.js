52898b2f5ba9c94389f1c62acc37b3d848d6853d
Hubert <hubertwojnarowicz01@gmail.com>
2022-03-02 21:46:13 +0100

    Create project architecture + add font

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+export default function App() {
+  return <Bla>hello</Bla>;
+}
+
+const Bla = styled.h2`
+  font-weight: 700;
+`;
---
+++ src/components/SuperHeader/SuperHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+function SuperHeader() {
+  return <div>SuperHeader</div>;
+}
+
+export default SuperHeader;
+
+const SuperHeaderFullWidth = styled.div`
+  height: 36px
+  background-color: 
+`;
+
+const SuperHeaderWrapper = styled.div`
+  max-width: 1920px;
+`;
