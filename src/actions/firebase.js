5541486f52efd023247ca1d50ea8e547d929f1b7
Hubert <hubertwojnarowicz01@gmail.com>
2022-03-22 14:38:27 +0100

    Fix the user display name

---
+++ src/actions/firebase.js
@@ -0,0 +1,13 @@
+import { app, auth, db } from "../lib/firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
+
+export const getUserName = async (user) => {
+  const result = query(collection(db, "users"), where("uid", "==", user.uid));
+  const docs = await getDocs(result);
+
+  const userName = await docs.docs[0]._document.data.value.mapValue.fields
+    .username;
+  const arrName = Object.values(userName);
+  const nameToString = arrName.toString();
+  return nameToString;
+};
