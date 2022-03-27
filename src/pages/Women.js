58a2420439a9e4a9593633de8ec80269e2cedd64
hubertwojnarowicz <76003091+hjubsoni@users.noreply.github.com>
2022-03-27 22:05:15 +0200

    Update Women.js

--- src/pages/Women.js
+++ src/pages/Women.js
@@ -1,29 +1,38 @@
-import React from "react";
-import SuperHeader from "../components/SuperHeader";
-import Header from "../components/Header/Header";
-import Shoe from "../components/Shoe";
-import { AllShoes } from "../data/data";
-import styled from "styled-components/macro";
-import Footer from "../components/Footer";
+import React from 'react';
+import SuperHeader from '../components/SuperHeader';
+import Header from '../components/Header/Header';
+import Shoe from '../components/Shoe';
+import { AllShoes } from '../data/data';
+import styled from 'styled-components/macro';
+import Footer from '../components/Footer';
+import { COLORS, QUERIES } from '../variables';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
 
 function Women() {
   const getWomensShoes = AllShoes.filter(({ forWho }) => {
-    return forWho === "Women";
+    return forWho === 'Women';
   });
 
   return (
     <>
       <SuperHeader />
       <Header />
-      <NewReleasesWrapper>
-        {getWomensShoes.map((boot) => {
-          return (
-            <ShoeWrapper key={boot.index}>
-              <Shoe {...boot} />
-            </ShoeWrapper>
-          );
-        })}
-      </NewReleasesWrapper>
+      <MaxWidthWrapper>
+        <DirectionWrapper>
+          <DirectionLinks to={ROUTES.DASHBOARD}>Home</DirectionLinks>
+          <DirectionLinks to={ROUTES.WOMEN}>Women</DirectionLinks>
+        </DirectionWrapper>
+        <NewReleasesWrapper>
+          {getWomensShoes.map((boot) => {
+            return (
+              <ShoeWrapper key={boot.index}>
+                <Shoe {...boot} />
+              </ShoeWrapper>
+            );
+          })}
+        </NewReleasesWrapper>
+      </MaxWidthWrapper>
       <Footer />
     </>
   );
@@ -31,13 +40,59 @@ function Women() {
 
 export default Women;
 
+const MaxWidthWrapper = styled.main`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 32px;
+`;
+
+const DirectionWrapper = styled.div`
+  max-width: 1920px;
+  width: 100%;
+  display: flex;
+  gap: 24px;
+  margin-left: 0;
+  margin-bottom: 32px;
+
+  position: relative;
+
+  &:before {
+    content: '/';
+    position: absolute;
+    left: 67px;
+    color: ${COLORS.gray[300]};
+    font-size: 1.4rem;
+  }
+`;
+
+const DirectionLinks = styled(Link)`
+  text-decoration: none;
+  color: ${COLORS.gray[300]};
+  font-size: 1.4rem;
+
+  &:hover {
+    color: ${COLORS.gray[700]};
+    transition: 300ms ease-in;
+  }
+`;
+
 const NewReleasesWrapper = styled.main`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(min(375px, 100%), 1fr));
   gap: 32px;
-  margin: 32px;
+  max-width: 1920px;
+  width: 100%;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  }
 `;
 
 const ShoeWrapper = styled.div`
   min-width: 275px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    min-width: 225px;
+  }
 `;
