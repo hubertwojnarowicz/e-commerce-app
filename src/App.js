04c19c2497523ffe4d91bd4de19353250f453a91
Hubert <hubertwojnarowicz01@gmail.com>
2022-03-30 20:59:23 +0200

    Pass cart provider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import * as ROUTES from "./constants/routes";
-import ReactLoader from "./components/Loader";
-import UserContext from "./context/user";
-import useAuthListener from "./hooks/use-auth-listener";
-
-const Dashboard = lazy(() => import("./pages/Dashboard"));
-const Men = lazy(() => import("./pages/Men"));
-const Women = lazy(() => import("./pages/Women"));
-const Kids = lazy(() => import("./pages/Kids"));
-const Cart = lazy(() => import("./pages/Cart"));
-const NewReleases = lazy(() => import("./pages/NewReleases"));
-const Favourites = lazy(() => import("./pages/Favourites"));
-const SignIn = lazy(() => import("./pages/SignIn"));
-const SignUp = lazy(() => import("./pages/SignUp"));
-const Sale = lazy(() => import("./pages/Sale"));
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import ReactLoader from './components/Loader';
+import UserContext from './context/user';
+import useAuthListener from './hooks/use-auth-listener';
+import { CartProvider } from 'react-use-cart';
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Men = lazy(() => import('./pages/Men'));
+const Women = lazy(() => import('./pages/Women'));
+const Kids = lazy(() => import('./pages/Kids'));
+const Cart = lazy(() => import('./pages/Cart'));
+const NewReleases = lazy(() => import('./pages/NewReleases'));
+const Favourites = lazy(() => import('./pages/Favourites'));
+const SignIn = lazy(() => import('./pages/SignIn'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const Sale = lazy(() => import('./pages/Sale'));
 const Shoes = lazy(() => import('./pages/Shoes'));
 
 export default function App() {
@@ -22,23 +22,25 @@ export default function App() {
 
   return (
     <UserContext.Provider value={{ user }}>
-      <Router>
-        <Suspense fallback={<ReactLoader />}>
-          <Routes>
-            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-            <Route path={ROUTES.MEN} element={<Men />} />
-            <Route path={ROUTES.WOMEN} element={<Women />} />
-            <Route path={ROUTES.KIDS} element={<Kids />} />
-            <Route path={ROUTES.CART} element={<Cart />} />
-            <Route path={ROUTES.NEWRELEASES} element={<NewReleases />} />
-            <Route path={ROUTES.FAVOURITES} element={<Favourites />} />
-            <Route path={ROUTES.SIGNIN} element={<SignIn />} />
-            <Route path={ROUTES.SIGNUP} element={<SignUp />} />
-            <Route path={ROUTES.SALE} element={<Sale />} />
-            <Route path={ROUTES.SHOE} element={<Shoes />} />
-          </Routes>
-        </Suspense>
-      </Router>
+      <CartProvider>
+        <Router>
+          <Suspense fallback={<ReactLoader />}>
+            <Routes>
+              <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+              <Route path={ROUTES.MEN} element={<Men />} />
+              <Route path={ROUTES.WOMEN} element={<Women />} />
+              <Route path={ROUTES.KIDS} element={<Kids />} />
+              <Route path={ROUTES.CART} element={<Cart />} />
+              <Route path={ROUTES.NEWRELEASES} element={<NewReleases />} />
+              <Route path={ROUTES.FAVOURITES} element={<Favourites />} />
+              <Route path={ROUTES.SIGNIN} element={<SignIn />} />
+              <Route path={ROUTES.SIGNUP} element={<SignUp />} />
+              <Route path={ROUTES.SALE} element={<Sale />} />
+              <Route path={ROUTES.SHOE} element={<Shoes />} />
+            </Routes>
+          </Suspense>
+        </Router>
+      </CartProvider>
     </UserContext.Provider>
   );
 }
