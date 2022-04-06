001a4016d5b13b01a625bce28c738b8e1ababa19
Hubert <hubertwojnarowicz01@gmail.com>
2022-04-06 17:55:45 +0200

    Add cart provider + wrapper to set height based on % in other components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ReactLoader from './components/Loader';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
 import { CartProvider } from 'react-use-cart';
+import styled from 'styled-components/macro';
+
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 const Men = lazy(() => import('./pages/Men'));
 const Women = lazy(() => import('./pages/Women'));
@@ -23,24 +25,32 @@ export default function App() {
   return (
     <UserContext.Provider value={{ user }}>
       <CartProvider>
-        <Router>
-          <Suspense fallback={<ReactLoader />}>
-            <Routes>
-              <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-              <Route path={ROUTES.MEN} element={<Men />} />
-              <Route path={ROUTES.WOMEN} element={<Women />} />
-              <Route path={ROUTES.KIDS} element={<Kids />} />
-              <Route path={ROUTES.CART} element={<Cart />} />
-              <Route path={ROUTES.NEWRELEASES} element={<NewReleases />} />
-              <Route path={ROUTES.FAVOURITES} element={<Favourites />} />
-              <Route path={ROUTES.SIGNIN} element={<SignIn />} />
-              <Route path={ROUTES.SIGNUP} element={<SignUp />} />
-              <Route path={ROUTES.SALE} element={<Sale />} />
-              <Route path={ROUTES.SHOE} element={<Shoes />} />
-            </Routes>
-          </Suspense>
-        </Router>
+        <Wrapper>
+          <Router>
+            <Suspense fallback={<ReactLoader />}>
+              <Routes>
+                <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+                <Route path={ROUTES.MEN} element={<Men />} />
+                <Route path={ROUTES.WOMEN} element={<Women />} />
+                <Route path={ROUTES.KIDS} element={<Kids />} />
+                <Route path={ROUTES.CART} element={<Cart />} />
+                <Route path={ROUTES.NEWRELEASES} element={<NewReleases />} />
+                <Route path={ROUTES.FAVOURITES} element={<Favourites />} />
+                <Route path={ROUTES.SIGNIN} element={<SignIn />} />
+                <Route path={ROUTES.SIGNUP} element={<SignUp />} />
+                <Route path={ROUTES.SALE} element={<Sale />} />
+                <Route path={ROUTES.SHOE} element={<Shoes />} />
+              </Routes>
+            </Suspense>
+          </Router>
+        </Wrapper>
       </CartProvider>
     </UserContext.Provider>
   );
 }
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
