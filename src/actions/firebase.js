657e0201f1b994b15bd4ed769d659549020a1824
Hubert <hubertwojnarowicz01@gmail.com>
2022-04-06 18:04:57 +0200

    Fix firebase rules for production mode

--- src/actions/firebase.js
+++ src/actions/firebase.js
@@ -1,8 +1,8 @@
-import { app, auth, db } from "../lib/firebase";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import { db } from '../lib/firebase';
+import { query, collection, getDocs, where } from 'firebase/firestore';
 
 export const getUserName = async (user) => {
-  const result = query(collection(db, "users"), where("uid", "==", user.uid));
+  const result = query(collection(db, 'users'), where('uid', '==', user.uid));
   const docs = await getDocs(result);
 
   const userName = await docs.docs[0]._document.data.value.mapValue.fields
